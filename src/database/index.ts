5ea80781f69b47188e16d1fa50cd2c8023282a6f
ySnoopyDogy <CommandPlayermine@gmail.com>
2021-10-22 16:40:08 -0300

    fix: database connextion

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 
 export default () => {
-  mongoose.connect("mongodb://mongodb:27018/db");
-
-  mongoose.connection.once("once", () => {
-    console.log("[DATABASE] Connected succesfully");
-  });
+  mongoose
+    .connect("mongodb://database:27017/coords")
+    .then(() => console.log("[DATABASE] Connected Succesfully"))
+    .catch((e) => console.error(`[DATABASE] Error when connecting, ${e}`));
 };
